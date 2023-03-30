92e72135fd36a75ebeada1c48b628a343f2ebb14
vishnu-punwani12 <vishnupunwani20@gmail.com>
2023-03-29 20:18:27 -0700

    finished for now

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-const wordSubmitted = document.querySelector('.inputs input');
+const submittedWord = document.querySelector(".inputs input");
+checkButton = document.querySelector(".inputs button");
 infoTxt = document.querySelector(".info-txt");
-checkButton = document.querySelector('.inputs button');
 let filterInput;
 
+// Took the variable called reverseInput and set that 
+// to the filtered word after it had been split, reversed and rejoined again 
 
-
-
+//then I put a conditional saying that if the filtered word was NOT equal to the new reversed input
+//then return the inputted word with the phrase isn't a palindrome
 
 checkButton.addEventListener("click", () => {
-    let reverseInput = filterInput.split("").reverse().join(" ");
+    let reverseInput = filterInput.split("").reverse().join("");
     infoTxt.style.display = "block";
     if(filterInput != reverseInput) {
-        return infoTxt.innerHTML = `No, <span>'${txtInput.value}'</span> isn't a palindrome!`;
+        return infoTxt.innerHTML = `No, <span>'${submittedWord.value}'</span> isn't a palindrome!`;
     }
-    infoTxt.innerHTML = `Yes, <span>'${txtInput.value}'</span> is a palindrome!`;
+    infoTxt.innerHTML = `Yes, <span>'${submittedWord.value}'</span> is a palindrome!`;
+});
+
+// this section here was dedicated to setting up the word and filtering it
+// so here i took the original word and get rid of the special characters and spaces 
+
+
+submittedWord.addEventListener("keyup", () => {
+    filterInput = submittedWord.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
+
+    if(filterInput) {
+        return checkButton.classList.add("active");
+    }
+    infoTxt.style.display = "none";
+    checkButton.classList.remove("active");
 });
 
 
 
-
-
-wordSubmitted.addEventListener('keyup', () => {
-    let filterInput = wordSubmitted.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
-    if (filterInput){
-        return checkButton.classList.add('active');
-    }    
-    infoTxt.style.display = "none";
-    checkButton.classList.remove('active');
-})
