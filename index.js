3dbb2a64fb89d50dfbf3e1752286ba49f9079afa
vishnu-punwani12 <vishnupunwani20@gmail.com>
2023-03-29 19:08:16 -0700

    submit button listener

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const wordSubmitted = document.querySelector('.inputs input');
+checkButton = document.querySelector('.inputs button');
+let filterInput;
+
+checkButton.addEventListener('click',() => {
+    console.log(filterInput);
+})
 
 wordSubmitted.addEventListener('keyup', () => {
     let filterInput = wordSubmitted.value.replace(/[^A-Z0-9]/ig, '');
-
-    console.log(wordSubmitted.value)
+    if (filterInput){
+        return checkButton.classList.add('active');
+    }    
+    checkButton.classList.remove('active');
 })
